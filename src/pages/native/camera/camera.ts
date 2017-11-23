e74a6e1e1d160b47dac3c287af2503e6bea95011
16262824 <16262824@qq.com>
2017-11-24 00:28:09 +0800

    camera

--- src/pages/native/camera/camera.ts
+++ src/pages/native/camera/camera.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the CameraPage page.
  *
@@ -14,10 +14,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'camera.html',
 })
 export class CameraPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  base64Image: string;
+  url:string;
+  options: CameraOptions ;
+  constructor(private camera: Camera) { 
+    this.options = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+  }
+  
+  getCamera(){
+    this.options.destinationType=this.camera.DestinationType.DATA_URL;
+    this.options.mediaType=this.camera.MediaType.PICTURE;
+    this.options.sourceType=this.camera.PictureSourceType.CAMERA;
+    this.camera.getPicture(this.options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
+  getPhotoLib(){
+    this.options.destinationType=this.camera.DestinationType.DATA_URL;
+    this.options.mediaType=this.camera.MediaType.PICTURE;
+    this.options.sourceType=this.camera.PictureSourceType.PHOTOLIBRARY;
+    
+    this.camera.getPicture(this.options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+  getVideo(){
+    this.options.destinationType=this.camera.DestinationType.FILE_URI;
+    this.options.mediaType=this.camera.MediaType.VIDEO;
+    this.options.sourceType=this.camera.PictureSourceType.CAMERA;
+    
+    this.camera.getPicture(this.options).then((url) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.url = url;
+    }, (err) => {
+     // Handle error
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraPage');
   }
